5bb71f7eb787b315fe82475082c6c34616fc07f9
ryowk <r.wakatsuki1207@gmail.com>
2022-05-13 10:10:32 +0900

    delete unused

--- download_from_s3.sh
+++
@@ -1,7 +0,0 @@
-MODEL_DIRS=`ls -d */ | grep _model`
-
-for model_dir in $MODEL_DIRS
-do
-  echo $model_dir
-  aws s3 sync s3://pigimaru-kaggle-days-2021/championship-1/${model_dir} ${model_dir}output/
-done
